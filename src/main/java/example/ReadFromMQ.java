d137d9b5906f45da5e581d14730410d24d4e94e4
fafaform <006543@g-able.com>
2020-07-06 17:23:11 +0700

    edit broker and topic and edit receiving raw to json

--- src/main/java/example/ReadFromMQ.java
+++ src/main/java/example/ReadFromMQ.java
@@ -1,8 +1,11 @@
 package example;
 
 import org.apache.flink.api.common.functions.FilterFunction;
+import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.serialization.AbstractDeserializationSchema;
+import org.apache.flink.api.java.tuple.Tuple3;
 import org.apache.flink.api.java.tuple.Tuple6;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSink;
@@ -10,6 +13,7 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
+import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema;
 import org.apache.flink.util.Collector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,13 +23,24 @@ import java.util.Arrays;
 import java.util.Properties;
 
 public class ReadFromMQ {
+
     //// VARIABLES
-    public static String KAFKA_CONSUMER_TOPIC = "mq-sample100";
-    public static String KAFKA_PRODUCER_TOPIC = "flink-from-kafka";
+    public static String KAFKA_CONSUMER_TOPIC = "input";
+//    public static String KAFKA_CONSUMER_TOPIC = "flink-from-kafka";
+    public static String KAFKA_MASTER_PRODUCER_TOPIC = "CREDIT_CARD_MASTER_500";
+    public static String KAFKA_VISA_PRODUCER_TOPIC = "CREDIT_CARD_VISA_500";
     //// TEST IN CLUSTER
-    public static String BOOTSTRAP_SERVER = "poc01.kbtg:9092,poc02.kbtg:9092,poc03.kbtg:9092";
+    public static String BOOTSTRAP_SERVER = "172.30.74.84:9092,172.30.74.85:9092,172.30.74.86:9092";
+//    public static String BOOTSTRAP_SERVER = "poc01.kbtg:9092,poc02.kbtg:9092,poc03.kbtg:9092";
     //// TEST IN MY LOCAL
 //    public static String BOOTSTRAP_SERVER = "localhost:9092";
+
+    public static String TXN_ID = "TXN_ID";
+    public static String TIMESTAMP = "TIMESTAMP";
+    public static String CARD_TYPE = "CARD_TYPE";
+    public static String CARD_STATUS = "CARD_STATUS";
+    public static String TXN_AMT = "TXN_AMT";
+    public static String CARD_NUMBER = "CARD_NUMBER";
 
     public static Logger LOG = LoggerFactory.getLogger(ReadFromMQ.class);
 
@@ -40,30 +55,49 @@ public class ReadFromMQ {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-        FlinkKafkaProducer<String> myProducer = new FlinkKafkaProducer(KAFKA_PRODUCER_TOPIC, new ProducerStringSerializationSchema(KAFKA_PRODUCER_TOPIC), properties, FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
-        FlinkKafkaConsumer<byte[]> kafkaSource = new FlinkKafkaConsumer(KAFKA_CONSUMER_TOPIC, new AbstractDeserializationSchema<byte[]>() {
+        FlinkKafkaProducer<String> masterProducer = new FlinkKafkaProducer(KAFKA_MASTER_PRODUCER_TOPIC, new ProducerStringSerializationSchema(KAFKA_MASTER_PRODUCER_TOPIC), properties, FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
+        FlinkKafkaProducer<String> visaProducer = new FlinkKafkaProducer(KAFKA_VISA_PRODUCER_TOPIC, new ProducerStringSerializationSchema(KAFKA_VISA_PRODUCER_TOPIC), properties, FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
+
+        ////////////////////////// RECEIVE JSON
+        //// RECEIVE JSON
+        FlinkKafkaConsumer<ObjectNode> JsonSource = new FlinkKafkaConsumer(KAFKA_CONSUMER_TOPIC, new JSONKeyValueDeserializationSchema(false), properties);
+        DataStream<Tuple6<Long, Long, String, String, Double, String>> messageStream = env.addSource(JsonSource).flatMap(new FlatMapFunction<ObjectNode, Tuple6<Long, Long, String, String, Double, String>>() {
             @Override
-            public byte[] deserialize(byte[] bytes) throws IOException {
-                return bytes;
-            }
-        }, properties);
-
-        //// CONVERTING PROCESS
-        DataStream<Tuple6<Long, Long, String, String, Double, String>> messageStream = env.addSource(kafkaSource).process(new ProcessFunction<byte[], Tuple6<Long, Long, String, String, Double, String>>() {
-            @Override
-            public void processElement(byte[] s, Context context, Collector<Tuple6<Long, Long, String, String, Double, String>> collector) throws Exception {
-
-                TransactionObject transactionObject = new TransactionObject();
-                transactionObject.setTXN_ID(Arrays.copyOfRange(s, 0, 9));
-                transactionObject.setTIMESTAMP(Arrays.copyOfRange(s, 35, 45));
-                transactionObject.setCARD_TYPE(Arrays.copyOfRange(s, 25, 26));
-                transactionObject.setCARD_STATUS(Arrays.copyOfRange(s, 26, 27));
-                transactionObject.setTXN_AMT(Arrays.copyOfRange(s, 27, 35));
-                transactionObject.setCARD_NUMBER(Arrays.copyOfRange(s, 9, 25));
-
-                collector.collect(new Tuple6<Long, Long, String, String, Double, String>(transactionObject.getTXN_ID(), transactionObject.getTIMESTAMP(), transactionObject.getCARD_TYPE(), transactionObject.getCARD_STATUS(), transactionObject.getTXN_AMT(), transactionObject.getCARD_NUMBER()));
+            public void flatMap(ObjectNode s, Collector<Tuple6<Long, Long, String, String, Double, String>> collector) throws Exception {
+                collector.collect(new Tuple6<Long, Long, String, String, Double, String>(s.get("value").get(TXN_ID).asLong(),
+                        s.get("value").get(TIMESTAMP).asLong(),
+                        s.get("value").get(CARD_TYPE).asText(),
+                        s.get("value").get(CARD_STATUS).asText(),
+                        s.get("value").get(TXN_AMT).asDouble(),
+                    s.get("value").get(CARD_NUMBER).asText()));
             }
         });
+        //////////////////////////
+        ////////////////////////// RECIEVE BINARY
+//        FlinkKafkaConsumer<byte[]> kafkaSource = new FlinkKafkaConsumer(KAFKA_CONSUMER_TOPIC, new AbstractDeserializationSchema<byte[]>() {
+//            @Override
+//            public byte[] deserialize(byte[] bytes) throws IOException {
+//                return bytes;
+//            }
+//        }, properties);
+//
+//        //// CONVERTING PROCESS
+//        DataStream<Tuple6<Long, Long, String, String, Double, String>> messageStream = env.addSource(kafkaSource).process(new ProcessFunction<byte[], Tuple6<Long, Long, String, String, Double, String>>() {
+//            @Override
+//            public void processElement(byte[] s, Context context, Collector<Tuple6<Long, Long, String, String, Double, String>> collector) throws Exception {
+//
+//                TransactionObject transactionObject = new TransactionObject();
+//                transactionObject.setTXN_ID(Arrays.copyOfRange(s, 0, 9));
+//                transactionObject.setTIMESTAMP(Arrays.copyOfRange(s, 35, 45));
+//                transactionObject.setCARD_TYPE(Arrays.copyOfRange(s, 25, 26));
+//                transactionObject.setCARD_STATUS(Arrays.copyOfRange(s, 26, 27));
+//                transactionObject.setTXN_AMT(Arrays.copyOfRange(s, 27, 35));
+//                transactionObject.setCARD_NUMBER(Arrays.copyOfRange(s, 9, 25));
+//
+//                collector.collect(new Tuple6<Long, Long, String, String, Double, String>(transactionObject.getTXN_ID(), transactionObject.getTIMESTAMP(), transactionObject.getCARD_TYPE(), transactionObject.getCARD_STATUS(), transactionObject.getTXN_AMT(), transactionObject.getCARD_NUMBER()));
+//            }
+//        });
+        //////////////////////////
 
         //// CREDIT_CARD_MASTER
         DataStreamSink<String> CREDIT_CARD_MASTER = messageStream.filter(new FilterFunction<Tuple6<Long, Long, String, String, Double, String>>() {
@@ -82,7 +116,7 @@ public class ReadFromMQ {
                 LOG.info("CREDIT_CARD_MASTER: " + result);
                 collector.collect(result);
             }
-        }).rebalance().print();
+        }).rebalance().addSink(masterProducer);
 
 
 
@@ -103,22 +137,22 @@ public class ReadFromMQ {
                 LOG.info("CREDIT_CARD_MASTER: " + result);
                 collector.collect(result);
             }
-        }).rebalance().print();
+        }).rebalance().addSink(visaProducer);
 
 
         //// SINK TO KAFKA
-        DataStreamSink<String> sendingToKafka = messageStream.process(new ProcessFunction<Tuple6<Long, Long, String, String, Double, String>, String>() {
-            @Override
-            public void processElement(Tuple6<Long, Long, String, String, Double, String> transactionData, Context context, Collector<String> collector) throws Exception {
-                collector.collect("{\"TXN_ID\":" + transactionData.f0
-                        + ",\"TIMESTAMP\":" + transactionData.f1
-                        + ",\"CARD_TYPE\":\"" + transactionData.f2 +"\""
-                        + ",\"CARD_STATUS\":\"" + transactionData.f3 +"\""
-                        + ",\"TXN_AMT\":" + transactionData.f4
-                        + ",\"CARD_NUMBER\":\"" + transactionData.f5 + "\""
-                        + "}");
-            }
-        }).rebalance().addSink(myProducer);
+//        DataStreamSink<String> sendingToKafka = messageStream.process(new ProcessFunction<Tuple6<Long, Long, String, String, Double, String>, String>() {
+//            @Override
+//            public void processElement(Tuple6<Long, Long, String, String, Double, String> transactionData, Context context, Collector<String> collector) throws Exception {
+//                collector.collect("{\"TXN_ID\":" + transactionData.f0
+//                        + ",\"TIMESTAMP\":" + transactionData.f1
+//                        + ",\"CARD_TYPE\":\"" + transactionData.f2 +"\""
+//                        + ",\"CARD_STATUS\":\"" + transactionData.f3 +"\""
+//                        + ",\"TXN_AMT\":" + transactionData.f4
+//                        + ",\"CARD_NUMBER\":\"" + transactionData.f5 + "\""
+//                        + "}");
+//            }
+//        }).rebalance().addSink(myProducer);
 
         env.execute("Read from MQ");
     }
